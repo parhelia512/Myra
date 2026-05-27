5976e139cb502523628ad1b221d411c2ef108b82
Roman Shapiro <eleroman@mail.ru>
2026-05-27 07:24:10 +0700

    Updated some references versions

--- src/Myra/Graphics2D/UI/InputEventsManager.cs
+++ src/Myra/Graphics2D/UI/InputEventsManager.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
+﻿using Myra.Events;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Myra.Graphics2D.UI
 {
 	public enum InputEventType
-    {
-        None = -1,
-        MouseLeft,
+	{
+		None = -1,
+		MouseLeft,
 		MouseEntered,
 		MouseMoved,
 		MouseWheel,
@@ -19,28 +19,28 @@ namespace Myra.Graphics2D.UI
 		TouchDoubleClick,
 		KeyUp,
 		KeyDown,
-        Closing,
-        KeyboardFocusLosing,
-        ContextMenuClosing,
-        CharInput,
-        TextDeleted,
-        CursorPositionChanged,
-        TextChangedByUser,
-        TextChanged,
-        SelectedIndexChanged,
-        HoverIndexChanged,
-        ProportionChanged,
-        EnabledChanged,
-        KeyboardFocusChanged,
-        ArrangeUpdated,
-        PlacedChanged,
-        VisibleChanged,
-        LocationChanged,
-        SizeChanged,
-        SelectionChanged,
-        ValueChanged,
-        PressedChanged
-    }
+		Closing,
+		KeyboardFocusLosing,
+		ContextMenuClosing,
+		CharInput,
+		TextDeleted,
+		CursorPositionChanged,
+		TextChangedByUser,
+		TextChanged,
+		SelectedIndexChanged,
+		HoverIndexChanged,
+		ProportionChanged,
+		EnabledChanged,
+		KeyboardFocusChanged,
+		ArrangeUpdated,
+		PlacedChanged,
+		VisibleChanged,
+		LocationChanged,
+		SizeChanged,
+		SelectionChanged,
+		ValueChanged,
+		PressedChanged
+	}
 
 	internal interface IInputEventsProcessor
 	{
@@ -61,83 +61,87 @@ namespace Myra.Graphics2D.UI
 			}
 		}
 
-        private static readonly Queue<InputEvent> _eventsQueue = new Queue<InputEvent>();
+		private static readonly Queue<InputEvent> _eventsQueue = new Queue<InputEvent>();
 
-        private static readonly Stack<InputEvent> _eventsStack = new Stack<InputEvent>();
+		private static readonly Stack<InputEvent> _eventsStack = new Stack<InputEvent>();
 
-        public static void Queue(IInputEventsProcessor processor, InputEventType type)
+		public static void Queue(IInputEventsProcessor processor, InputEventType type)
 		{
 			var ev = new InputEvent(processor, type);
 
-            switch (MyraEnvironment.EventHandlingModel)
-            {
-                case Events.EventHandlingStrategy.EventCapturing:
-                    QueueEventsQueue(ev);
-                    break;
-                case Events.EventHandlingStrategy.EventBubbling:
-                    QueueEventsStack(ev);
-                    break;
-                default:
-                    break;
-            }
-        }
+			switch (MyraEnvironment.EventHandlingModel)
+			{
+				case EventHandlingStrategy.EventCapturing:
+					QueueEventsQueue(ev);
+					break;
+				case EventHandlingStrategy.EventBubbling:
+					QueueEventsStack(ev);
+					break;
+				default:
+					break;
+			}
+		}
 
 		private static void QueueEventsQueue(InputEvent ev)
-        {
-            _eventsQueue.Enqueue(ev);
-        }
+		{
+			_eventsQueue.Enqueue(ev);
+		}
 
-        private static void QueueEventsStack(InputEvent ev)
-        {
+		private static void QueueEventsStack(InputEvent ev)
+		{
 			_eventsStack.Push(ev);
-        }
+		}
 
-        public static void ProcessEvents()
+		public static void ProcessEvents()
 		{
 			switch (MyraEnvironment.EventHandlingModel)
 			{
-				case Events.EventHandlingStrategy.EventCapturing:
+				case EventHandlingStrategy.EventCapturing:
 					ProcessEventsQueue();
-                    break;
-				case Events.EventHandlingStrategy.EventBubbling:
-                    ProcessEventsStack();
-                    break;
+					break;
+				case EventHandlingStrategy.EventBubbling:
+					ProcessEventsStack();
+					break;
 				default:
 					break;
 			}
 		}
 
 		private static void ProcessEventsQueue()
-        {
-            while (_eventsQueue.Count > 0)
-            {
-                var ev = _eventsQueue.Dequeue();
-                ProcessEventInternal(ev);
-            }
-        }
-
-        private static void ProcessEventsStack()
-        {
-            while (_eventsStack.Count > 0)
-            {
-                var ev = _eventsStack.Pop();
+		{
+			while (_eventsQueue.Count > 0)
+			{
+				var ev = _eventsQueue.Dequeue();
 				ProcessEventInternal(ev);
-            }
-        }
+			}
+		}
+
+		private static void ProcessEventsStack()
+		{
+			while (_eventsStack.Count > 0)
+			{
+				var ev = _eventsStack.Pop();
+				ProcessEventInternal(ev);
+			}
+		}
 
 		private static void ProcessEventInternal(InputEvent ev)
 		{
 			ev.Processor.ProcessEvent(ev.Type);
 
-        }
+		}
 
-        private static IEnumerable<InputEvent> GetInputEvents() => MyraEnvironment.EventHandlingModel switch
-        {
-            Events.EventHandlingStrategy.EventBubbling => _eventsStack,
-            _ => _eventsQueue,
-        };
+		private static IEnumerable<InputEvent> GetInputEvents()
+		{
+			if (MyraEnvironment.EventHandlingModel == EventHandlingStrategy.EventBubbling)
+			{
+				return _eventsStack;
+			}
 
-        public static void StopPropagation(InputEventType eventType)
+			return _eventsQueue;
+		}
+
+		public static void StopPropagation(InputEventType eventType)
 		{
 			var events = GetInputEvents()
 				.Where(x => x.Type != eventType)
@@ -145,29 +149,29 @@ namespace Myra.Graphics2D.UI
 
 			SetInputEvents(events);
 
-        }
+		}
 
 		private static void SetInputEvents(IEnumerable<InputEvent> events)
 		{
 			switch (MyraEnvironment.EventHandlingModel)
 			{
-				case Events.EventHandlingStrategy.EventCapturing:
-                    _eventsQueue.Clear();
+				case EventHandlingStrategy.EventCapturing:
+					_eventsQueue.Clear();
 					foreach (var @event in events)
 					{
 						_eventsQueue.Enqueue(@event);
 					}
 					break;
-				case Events.EventHandlingStrategy.EventBubbling:
-                    _eventsStack.Clear();
-                    foreach (var @event in events)
-                    {
-                        _eventsStack.Push(@event);
-                    }
-                    break;
+				case EventHandlingStrategy.EventBubbling:
+					_eventsStack.Clear();
+					foreach (var @event in events)
+					{
+						_eventsStack.Push(@event);
+					}
+					break;
 				default:
 					break;
 			}
 		}
-    }
+	}
 }
